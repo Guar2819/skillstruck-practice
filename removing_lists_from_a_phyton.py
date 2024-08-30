ef3ebc33283c13c440fa2c83aceeef0a5a53205b
Guar2819 <jguar312@evit.edu>
2024-08-30 09:06:43 -0700

    exported from Skill Struck

---
+++ removing_lists_from_a_phyton.py
@@ -0,0 +1,5 @@
+# my_list = [int(n) for n in input("Input a list of numbers: ").split()]  # int() converts a string to an int,  for - in loops the list, input prompts user, .split() converts a string to a list
+
+("Input a list of beans: ") list_of_beans
+print(list_of_beans)
+print("Hello World")
