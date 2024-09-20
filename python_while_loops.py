b43d798ebf2aacb19766f54ab0d15bc5ca88ac80
Guar2819 <jguar312@evit.edu>
2024-09-20 07:50:42 -0700

    exported from Skill Struck

---
+++ python_while_loops.py
@@ -0,0 +1,19 @@
+# count = 0
+
+# number = int(input("Give me a number: "))
+
+# while number > 0:
+#     count += 1
+#     number = int(input("Give me a number"))
+
+#  print(count)
+
+# count = 0
+
+# number = int(input("Give me a number: "))
+
+# while number > 0:
+#     count += number
+#     number = int(input("Give me a number"))
+
+# print(count)
