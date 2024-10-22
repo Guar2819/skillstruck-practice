36ae5436e932ef5ca71c0352e77a90999283aa60
Guar2819 <jguar312@evit.edu>
2024-10-22 09:09:47 -0700

    exported from Skill Struck

---
+++ python_function_parameters.py
@@ -0,0 +1,17 @@
+#def odd_even(number):
+    #if number % 2 == 0:
+        #print("even")
+    #else:
+        #print("odd")
+
+#odd_even(5)
+#dd_even(10)
+#odd_even(7)
+
+#year = int(input("What year?"))
+
+#def leap_year(choice):
+    #if choice % 4 == 0 and choice % 100 !=0:
+        #print("Leap year!")
+    #else:
+        #print("Not a leap year.")
