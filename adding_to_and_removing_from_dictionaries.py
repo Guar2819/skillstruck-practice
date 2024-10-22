f7c96d1d248370e0a5b4902e07d013772350a867
Guar2819 <jguar312@evit.edu>
2024-10-22 09:09:50 -0700

    exported from Skill Struck

---
+++ adding_to_and_removing_from_dictionaries.py
@@ -0,0 +1,29 @@
+
+#shapes = {
+    #"Triangle": 8,
+    #"Circle": 15,
+    #"Square": 10,
+    #"Rectangle": 12
+#}
+
+#new_shape = input("Enter the name of the new shape: ")
+#new_height = int(input("Enter the height of the new shape in inches: "))
+
+#shapes[new_shape] = new_height
+
+#print(shapes)
+
+#trees = {
+    #"aspen": 75,
+    #"pine": 82,
+    #"maple": 60,
+    #"oak": 65,
+    #"willow": 80,
+    #"cottonwood": 100
+#}
+
+#tree_to_remove = input("Enter the name of the tree you want to remove: ")
+
+#trees.pop(tree_to_remove, None)  # 
+
+#print(trees)
