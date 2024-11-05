e473f5bf3ce6f0ac0c111c5006d757a9e4586964
Guar2819 <jguar312@evit.edu>
2024-11-05 09:55:27 -0700

    exported from Skill Struck

---
+++ dictionaries_continued.py
@@ -0,0 +1,31 @@
+#dictionary = {
+#    1: "bicycle",
+#    2: "soccer balls",
+#    3: "piano books"
+#}
+
+#dictionary[4] = input("What do you have 4 of? ")
+#dictionary[5] = input("What do you have 5 of? ")
+#dictionary[6] = input("What do you have 6 of? ")
+
+#print(dictionary)
+
+#shopping = {}
+
+#apples_need = int(input("How many apples do you need? "))
+#bananas_need = int(input("How many bananas do you need? "))
+#strawberries_need = int(input("How many strawberries do you need? "))
+
+#if apples_need > 5:
+#    apples_buy = apples_need - 5
+#    shopping["apples"] = apples_buy
+
+#if bananas_need > 7:
+#    bananas_buy = bananas_need - 7
+#    shopping["bananas"] = bananas_buy
+
+#if strawberries_need > 3:
+#    strawberries_buy = strawberries_need - 3
+#    shopping["strawberries"] = strawberries_buy
+
+#print(shopping)
