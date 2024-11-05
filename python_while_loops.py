f42a39bcfcc9e1eed53187b19497065f55115fdc
Guar2819 <jguar312@evit.edu>
2024-11-05 09:55:25 -0700

    exported from Skill Struck

--- python_while_loops.py
+++ python_while_loops.py
@@ -1,19 +1,11 @@
-# count = 0
+from datetime import date
 
-# number = int(input("Give me a number: "))
+# Create a date object for a specific date
+specific_date = date(2024, 10, 30)  # Year, Month, Day
 
-# while number > 0:
-#     count += 1
-#     number = int(input("Give me a number"))
+# Extract the month and day
+month = specific_date.month
+day = specific_date.day
 
-#  print(count)
-
-# count = 0
-
-# number = int(input("Give me a number: "))
-
-# while number > 0:
-#     count += number
-#     number = int(input("Give me a number"))
-
-# print(count)
+print(f"Specific month: {month}")
+print(f"Specific day: {day}")
