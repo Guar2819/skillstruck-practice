eebdf3d5496910bd64dc2df3d9f8cf8b48abaa48
Guar2819 <jguar312@evit.edu>
2024-11-08 09:28:15 -0700

    exported from Skill Struck

--- python_while_loops.py
+++ python_while_loops.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 # Create a date object for a specific date
-specific_date = date(2024, 10, 30)  # Year, Month, Day
+specific_date = date(2024, 10, 29)  # Year, Month, Day
 
 # Extract the month and day
 month = specific_date.month
