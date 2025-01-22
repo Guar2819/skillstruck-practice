8d8c445089aa0f48e4678b4955831023568979b0
GuarA2819 <jguar312@evit.edu>
2025-01-22 10:25:51 -0700

    exported from Skill Struck

--- multiple_parameters_in_python_functions.py
+++ multiple_parameters_in_python_functions.py
@@ -1,5 +1,40 @@
-movies = 16
+# '''
+# ############  Lesson
+# def print_concatenated(param1, param2):
+#     # Print the concatenated string of both parameters
+#     print(param1 + " " + param2)
 
-while movies > 12
-   print(movies)
-   movies -= 1
+# # Call the function with different parameters
+# print_concatenated("Good", "Morning")
+# print_concatenated("Have", "a great day")
+
+# ############# Hungry for Apples 3
+# # Get user input for three different fruits
+# fruit1 = input("Enter the first fruit: ")
+# fruit2 = input("Enter the second fruit: ")
+# fruit3 = input("Enter the third fruit: ")
+
+# # Define the function that takes three parameters
+# def print_fruits(fruit1, fruit2, fruit3):
+#     # Print all fruits in one statement
+#     print(fruit1 + fruit2 + fruit3)
+
+# # Call the function with the user inputs
+# print_fruits(fruit1, fruit2, fruit3)
+
+
+# '''
+
+
+###############  Find the Smallest Integer Function
+#choice1 = int(input("What is the first number?"))
+#choice2 = int(input("What is the second number?"))
+
+#def my_function(choice1, choice2):
+#     if choice1 < choice2:
+#         print(choice1)
+#     else:
+#         print(choice2)
+
+# my_function(choice1, choice2)
+
