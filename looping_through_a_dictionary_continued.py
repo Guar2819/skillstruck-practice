0e08c50b61a23e6a8c8f46df50419bcbafe198c1
GuarA2819 <jguar312@evit.edu>
2025-01-23 08:09:19 -0700

    exported from Skill Struck

--- looping_through_a_dictionary_continued.py
+++ looping_through_a_dictionary_continued.py
@@ -1,1 +1,31 @@
-file = open("example.txt", "r")
+################  Lesson
+# dictionary_name = {
+#     "Key_1": value,
+#     "Key_2": value
+# }
+#  Keys can be "strings" or numbers (no quotes on numbers)
+#  Keys cannot repeat
+# Values can be "strings", numbers, or True/False
+# Values can repeat
+
+
+# ride = {
+#     "Alice": 120,    # "name": height
+#     "Bob": 80,
+#     "Charlie": 100,
+#     "Diana": 50,
+#     "Eve": 150
+# }
+
+# # dictionary_name.values() chooses the values
+
+# for height in ride.values():  
+#     if height >= 100:
+#         print("This person is tall enough to ride.")
+#     else:
+#        print("This person is too short to ride.")
+
+
+## TO DO: 
+## go look at the challenges and see if there was
+## prior code submitted
