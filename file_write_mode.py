c3262e0628538504a870f534bfbaab707abd3fc1
GuarA2819 <jguar312@evit.edu>
2025-02-04 10:31:18 -0700

    exported from Skill Struck

---
+++ file_write_mode.py
@@ -0,0 +1,12 @@
+# file = open("In short, I love to code", "w")
+# file.write("In short, I love to code!")
+# file.close()
+# open("porcupine.txt", "w")
+
+# file = open("Never mind", "w")
+# file.write("Never mind")
+
+# answer = input()
+# file = open("report.txt"), "w"
+# filename.write(answer)
+# print(file.read())
