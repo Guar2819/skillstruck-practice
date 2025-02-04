fddab9f2d07f4f73dad1f0fc0474415af38f7ff3
GuarA2819 <jguar312@evit.edu>
2025-02-04 10:31:19 -0700

    exported from Skill Struck

---
+++ nested_for_loops.py
@@ -0,0 +1,22 @@
+# rows = 3
+# pets = ["Dog", "Cat", "Bird", "Fish","Rabbit"]
+
+# list = [[pets for i in pets] for i in range(rows)]
+
+# print(list)
+
+# mylist = [1, 2, 3, 4, 5]
+
+# rows = int(input("How many rows do you want: ").strip())
+
+# list = [[(i * rows) for i in mylist] for i in range(1, rows + 1)]
+
+# print(list)
+
+# cols = ["windy", "breezy", "calm"]
+
+# rows = [item.strip() for item in input("Input a list of weather: ").split()]
+
+# list = [[(i + " " + j) for j in cols] for i in rows]
+
+# print(list)
