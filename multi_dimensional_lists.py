c360baf1979a9a5d1652e1d218aa5ae0891a9007
GuarA2819 <jguar312@evit.edu>
2025-02-04 10:31:19 -0700

    exported from Skill Struck

---
+++ multi_dimensional_lists.py
@@ -0,0 +1,34 @@
+# rows = 5
+# cols = 3
+# list = [
+
+# ]
+
+# for i in range(rows):
+#     var1 = [
+
+#     ]
+#     for j in range(cols):
+#         list.append(5)
+
+# print(list)
+
+# fruits = ["apple", "grape", "peach", "cinnamon", "vanilla"]
+
+# rows = [items.strip() for items in input("Inout a list of fruits").split()]
+
+# list = [[(i + " " + j) for j in fruits] for i in rows]
+
+# print(list)
+
+# first_name = ["Madeline", "Joseph", "Robert", "Alex"]
+# last_name = ["Platinum", "Hayden", "Smog", "Umpalumpa"]
+# list = []
+
+# for i in first_name:
+#     col = []
+#     for j in last_name:
+#         col.append(i + " " + j)
+#     list.append(col)
+
+# print(list)
