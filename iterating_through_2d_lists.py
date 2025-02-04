c2a28a424af7a98d41e1ced9326892d8017cff09
GuarA2819 <jguar312@evit.edu>
2025-02-04 10:31:20 -0700

    exported from Skill Struck

---
+++ iterating_through_2d_lists.py
@@ -0,0 +1,21 @@
+# my_list = [[40, 45, 50], [6, 7, 8], [100, 200, 300], [50, 60, 70], [9, 0, 1]]
+# rows = 5
+# cols = 3
+
+# for i in range(rows):
+#     for j in range(cols):
+#         my_list[i][j] = my_list[i][j] + 3
+
+# print(my_list)
+
+# my_list = [[0, 1, 2], [10, 15, 20], [100, 200, 300], [5, 6, 7]]
+# rows = 4
+# cols = 3
+
+# answer = int(input("What do you want to multiply by?"))
+
+# for i in range(rows):
+#     for j in range(cols):
+#         my_list[i][j] *= answer
+
+# print(my_list)
