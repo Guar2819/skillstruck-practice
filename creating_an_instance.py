3a672f78433a068654bba79f7b0fcf84f6aada6c
GuarA2819 <jguar312@evit.edu>
2025-02-06 18:25:46 -0700

    exported from Skill Struck

---
+++ creating_an_instance.py
@@ -0,0 +1,7 @@
+# class Hat:
+#     def __init__(self, kind, color, material):
+#         self.kind = kind
+#         self.color = color
+#         self.material = material
+
+# MyObject = Hat(kind, color, material)
