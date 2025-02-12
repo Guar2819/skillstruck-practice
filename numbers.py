ecee6a8b162c3f78593f9fd7f9cc82a2f73cea70
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:33:01 -0700

    exported from Skill Struck

---
+++ numbers.py
@@ -0,0 +1,17 @@
+# integer_var = 42
+# float_var = 3.14
+
+# feet_in_meters = 0.3048
+# print(feet_in_meters)
+
+# hamburger = 5
+# candy = 2
+# movie_ticket = 10
+# video_game = 20
+
+# store_price = 15.99
+# print(hamburger)
+# print(candy)
+# print(movie_ticket)
+# print(video_game)
+# print(store_price)
