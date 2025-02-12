640b0fc41c1deda67a0b83b740abbe1623cb17c6
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:33:00 -0700

    exported from Skill Struck

---
+++ variable_types.py
@@ -0,0 +1,19 @@
+# my_string = 'Hello, world!'
+# my_integer = 37
+# my_boolean = False
+# print(my_string)
+# print(my_integer)
+# print(my_boolean)
+
+# my_boolean = True
+# my_boolean = False
+
+# my_integer = 42
+# my_string = "Hello!"
+# my_boolean = True
+# print("my_integer")
+# print("my_string")
+# print("my_boolean")
+
+# age = input("How old are you? ")
+# print("You are " + age + " years old.")
