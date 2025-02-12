9ad80589adf9c5913a468516fad3500ae797272b
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:32:55 -0700

    exported from Skill Struck

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+# number = int(input('Give Number'))
+# arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+# arr.append(number)
+# for a in arr:
+#     for i in range(len(arr) - 1):
+#         if arr[i] > arr[i+1]:
+#             arr[i], arr[i+1] = arr[i+1], arr[i]
+# print(arr)
