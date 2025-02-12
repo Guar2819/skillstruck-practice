755585e6f5fe2db074a839ecb814002779e66260
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:33:00 -0700

    exported from Skill Struck

---
+++ syntax_comments.py
@@ -0,0 +1,6 @@
+morning = input('What do you want to do in the morning?')
+afternoon = input('What do you want to do in the afternoon?')
+evening = input('What do you want to do in the evening?')
+print(morning)
+print(afternoon)
+print(evening)
