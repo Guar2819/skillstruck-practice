13d57faecd28e6f7eac3790f6d227eec6f9799a3
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:32:47 -0700

    exported from Skill Struck

--- tuples.py
+++ tuples.py
@@ -1,4 +1,9 @@
-grades = (95, 70, 85, 92, 100)
-print(grades)
-print(grades[-2])
-print(grades[:3])
+# grades = (90, 85, 92, 88, 76, 95)
+
+# print(grades)
+# print(grades[-2])
+# print(grades[:3])
+
+# candies = (3, 5, 7, 8, 10, 2, 50, 4, 29, 14)
+
+# print(candies[-4:-1])
