227aeb08a787ebdedf13fef402bf87a0d9c61a61
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:32:51 -0700

    exported from Skill Struck

--- objects_continued.py
+++ objects_continued.py
@@ -1,3 +1,48 @@
-class = Vacation:
-    def __init__(self, place, distance, weather):
-        
+# class Vacation:
+#     def __init__(self, place, distance, weather):
+#         self.place = place
+#         self.distance = distance
+#         self.weather = weather
+
+#     def tuesday(self):
+#         print("We will be hiking on Tuesday.")
+
+# summer = Vacation("Hawaii", 2000, "Sunny")
+# summer.rating = 10
+# summer.weather = "rainy"
+
+# print(summer)
+# print(summer.rating)
+# print(summer.weather)
+
+# class Friday:
+#     def __init__(self, activity, friend):
+#         self.activity = activity
+#         self.friend = friend
+
+#     def pictures(self):
+#         print("We took so many pictures!")
+
+# thisWeekend = Friday("Movie", "Charlotte")
+# thisWeekend.money = 20
+# thisWeekend.friend = "Shane"
+
+# print(thisWeekend)
+# print(thisWeekend.money)
+# print(thisWeekend.friend)
+
+# class Shopping:
+#     def __init__(self, item, quality):
+#         self.item = item
+#         self.quality = quality
+#         self.total = []
+
+#     def spending(self, cost):
+#         self.total.append(cost)
+
+# sportStore = Shopping("Kayak", "High Quality")
+# sportStore.spending(500)
+# sportStore.spending(200)
+# sportStore.spending(150)
+
+# print(sportStore.total)
