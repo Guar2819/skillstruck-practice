20f3813cf84bdfc4d921aabfc95cdeddaf801b7f
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:32:51 -0700

    exported from Skill Struck

--- creating_an_instance.py
+++ creating_an_instance.py
@@ -5,3 +5,27 @@
 #         self.material = material
 
 # MyObject = Hat(kind, color, material)
+
+# class Pet:
+#     def __init__(self, name, species, age):
+#         self.name = name
+#         self.species = species
+#         self.age = age
+
+# pet1 = Pet("Buddy", "Dog", 3)
+# pet2 = Pet("Mittens", "Cat", 2)
+# pet3 = Pet("Goldie", "Fish", 1)
+
+# class Vacation:
+#     def __init__(self, location, activity, food):
+#         self.location = location
+#         self.activity = activity
+#         self.food = food
+
+# vacation1 = Vacation("Nevada", "Walked", "Chicken bake")
+# vacation2 = Vacation("Mexico", "La Bufadora", "Tacos de Adobada")
+# vacation3 = Vacation("California", "Swimming", "Pizza")
+
+# print("I went to " + vacation1.location + " for " + vacation1.activity + " and ate " + vacation1.food + ".")
+# print("I visited " + vacation2.location + " to do " + vacation2.activity + " and enjoyed " + vacation2.food + ".")
+# print("During my trip to " + vacation3.location + ", I went " + vacation3.activity + " and had " + vacation3.food + ".")
