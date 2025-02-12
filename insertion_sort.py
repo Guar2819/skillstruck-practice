b9c94e14ff0d729a9653470abd3ae836d2874194
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:32:57 -0700

    exported from Skill Struck

---
+++ insertion_sort.py
@@ -0,0 +1,11 @@
+# number = int(input('Give Number'))
+# lst = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+# lst.append(number)
+# for i in range(1, len(lst)):
+#     key = lst[i]
+#     j = i - 1
+#     while j >= 0 and key < lst[j]:
+#         lst[j + 1] = lst[j]
+#         j -= 1
+#     lst[j + 1] = key
+# print(lst)
