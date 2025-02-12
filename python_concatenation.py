df43dd25a88cb5cca02cfafdba979df6a187e636
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:33:03 -0700

    exported from Skill Struck

---
+++ python_concatenation.py
@@ -0,0 +1,22 @@
+
+# string1 = "Hello, "
+# string2 = "world"
+# string3 = "!"
+# concatenated_string = string1 + string2 + string3
+# print(concatenated_string)
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# birthday = input("Enter your birthday (e.g., ): ")
+# print(f"Hi {name}! You are {age} years old and you were born on {birthday}.")
+
+# integer_input = input("Enter an integer: ")
+# string_input = input("Enter a string: ")
+# print(integer_input + string_input)
+
+# animal = input("Enter an animal: ")
+# place = input("Enter a place: ")
+# noun = input("Enter a noun (plural): ")
+# adjective = input("Enter an adjective: ")
+# sentence = f"Today I rode a {animal} to the {place} to buy some {noun}. It was {adjective}."
+# print(sentence)
