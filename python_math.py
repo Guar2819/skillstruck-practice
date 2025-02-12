14ad47824a2da71fe78449e5d94ecda750fc5e10
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:33:04 -0700

    exported from Skill Struck

---
+++ python_math.py
@@ -0,0 +1,28 @@
+
+# caterpillars = 3
+# leaves = 25
+# print(caterpillars * leaves)
+
+# Take input from the user
+# input_number = input("Enter a number: ")
+
+# number = int(input_number)
+
+# number_less = number - 1
+# number_greater = number + 1
+
+# str_number_less = str(number_less)
+# str_number_greater = str(number_greater)
+
+# result = str_number_less + "," + str_number_greater
+# print(result)
+
+# num_people = input("Enter the number of people who will help: ")
+# amount_per_person = input("Enter the amount of money each person brings: ")
+
+# num_people = int(num_people)
+# amount_per_person = int(amount_per_person)
+
+# total_amount = num_people * amount_per_person
+
+# print(total_amount)
