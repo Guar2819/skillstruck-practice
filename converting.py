ba6fd59fea5c931020f46e6a842aa318c36ad7fc
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:33:02 -0700

    exported from Skill Struck

---
+++ converting.py
@@ -0,0 +1,24 @@
+# peppers = 2.8
+# peppers = int(2.8)
+# print(peppers)
+# number_string = "12345"
+
+# print(number_string)
+# number_string = "12345"
+
+# number_integer = int(number_string)
+
+# number_float = float(number_integer)
+
+# print(number_float)
+
+# decimal_input = input("Please enter a number with a decimal: ")
+# float_value = float(decimal_input)
+# int_value = int(float_value)
+# print(int_value)
+
+# name = input("Please enter your name: ")
+# age_input = input("Please enter your age: ")
+# age = int(age_input)
+# multiplied_name = name * age
+# print(multiplied_name)
