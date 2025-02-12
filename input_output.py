4f2efd27bade2e8d897ddd3602e1e6caa057db79
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:32:59 -0700

    exported from Skill Struck

---
+++ input_output.py
@@ -0,0 +1,8 @@
+# favorite_color = input("What is your favorite color? ")
+# print("Your favorite color is " + favorite_color)
+
+# favorite_dessert = input("What is your favorite dessert? ")
+# print(favorite_dessert)
+
+# name = input("Please enter your name: ")
+# print("Welcome to the website " + name)
