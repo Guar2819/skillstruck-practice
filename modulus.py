937baf7998b6a5a443deb6d2f6dd86a66cff39f4
GuarA2819 <jguar312@evit.edu>
2025-02-12 10:33:05 -0700

    exported from Skill Struck

---
+++ modulus.py
@@ -0,0 +1,20 @@
+
+# number = int(input("Enter a number: "))
+
+# remainder = number % 7
+
+# print(remainder)
+
+# number = int(input("Enter a number: "))
+
+# last_two_digits = number % 100
+
+# print(last_two_digits)
+
+# number = int(input("Enter a two-digit number: "))
+
+# tens_digit = number // 10
+
+# ones_digit = number % 10
+
+# print(tens_digit, ones_digit)
