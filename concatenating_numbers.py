81cce620067550d599c373f8af2a285d931e4534
GuarA2819 <jguar312@evit.edu>
2025-02-13 16:57:31 -0700

    exported from Skill Struck

---
+++ concatenating_numbers.py
@@ -0,0 +1,18 @@
+# string1 = "The number of miles we've hiked totals up to {}"
+# miles = 21
+# print(string1.format(miles))
+
+# apples_per_tree = float(input())
+
+# apples_per_wheelbarrow_one_tree = apples_per_tree / 3
+# total_apples_orchard = apples_per_tree * 8
+# apples_per_wheelbarrow_orchard = total_apples_orchard / 3
+
+# print("If Bob harvested one tree, he would have {:.1f} apples per wheelbarrow. If he harvested the whole orchard, he would have {:.1f} apples per wheelbarrow.".format(
+#     apples_per_wheelbarrow_one_tree, apples_per_wheelbarrow_orchard))
+
+# number_of_employees = int(input())
+
+# number_of_tables = (number_of_employees + 1) // 2
+
+# print("Minimum number of tables: {}".format(number_of_tables))
