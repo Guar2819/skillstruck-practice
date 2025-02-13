8b77c81fe94a68b00062946cf406810c0f51c623
GuarA2819 <jguar312@evit.edu>
2025-02-13 16:57:34 -0700

    exported from Skill Struck

---
+++ lists.py
@@ -0,0 +1,12 @@
+# candy = ['reeses', 'snickers', 'nerds', 'm&m']
+# pieces = [2, 4, 5, 1]
+# print(candy)
+# print(pieces)
+
+# friends = ['Andrew']
+# friends = ["Sam, Andrew, Camilla"]
+# print(friends)
+
+ages = [44, 47, 8, 23]
+
+print(ages)
