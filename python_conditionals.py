e9b2377c293d333dfaf1691ba0258aca5757aace
GuarA2819 <jguar312@evit.edu>
2025-02-13 16:57:36 -0700

    exported from Skill Struck

---
+++ python_conditionals.py
@@ -0,0 +1,17 @@
+
+# age = int(input("Enter your age: "))
+
+# license = input("Do you have a license? (yes or no): ").strip().lower() == "yes"
+
+# if age >= 16 and license:
+#     print("You are old enough to drive.")
+# else:
+#     print("You are not able to drive.")
+# license = True
+
+# height = int(input("Please enter your height in inches: "))
+
+# if height >= 48:
+#     print("You can ride the roller coaster.")
+# else:
+#     print("You can't ride the roller coaster.")
