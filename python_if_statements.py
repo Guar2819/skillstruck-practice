8fbfaee85a1342e8f0e6f22a2bae7a30de864ff5
GuarA2819 <jguar312@evit.edu>
2025-02-13 16:57:35 -0700

    exported from Skill Struck

---
+++ python_if_statements.py
@@ -0,0 +1,26 @@
+# number1 = 2
+# number2 = 5
+# print("The value of number1 is:", number1)
+# print("The value of number2 is:", number2)
+# number = 2
+# if number > 5:
+#     print("The number is greater than 2.")
+# number = 5
+# if number > 2:
+#     print("The number is greater than 2.")
+# else:
+#     print("The number is not greater than 2.")
+
+# number = int(input("Enter a positive number: "))
+
+# if 100 <= number < 1000:
+#     print("Yes")
+# else:
+#     print("No")
+
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
