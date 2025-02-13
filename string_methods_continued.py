efb473b747bd47d32994b5cf0a30fa0b68a4edc0
GuarA2819 <jguar312@evit.edu>
2025-02-13 16:57:30 -0700

    exported from Skill Struck

---
+++ string_methods_continued.py
@@ -0,0 +1,14 @@
+
+# string1 = "Hello, Python Programming World!"
+
+# sliced_string = string1[7:13]
+# print("Sliced string:", sliced_string)
+
+# split_string = string1.split()
+# print("Split string:", split_string)
+
+# length_of_string = len(string1)
+# print("Length of string:", length_of_string)
+
+# find_index = string1.find('P')
+# print("Index of 'P':", find_index)
