679158e1e878f1c16dc72ec97419f39332de9ac8
GuarA2819 <jguar312@evit.edu>
2025-02-13 16:57:29 -0700

    exported from Skill Struck

---
+++ string_methods.py
@@ -0,0 +1,25 @@
+
+# string1 = "Hello, World!"
+# string2 = "Python Programming"
+# string3 = "Replace me"
+
+# upper_string1 = string1.upper()
+# print("Uppercase of string1:", upper_string1)
+
+# lower_string2 = string2.lower()
+# print("Lowercase of string2:", lower_string2)
+
+# replaced_string3 = string3.replace("me", "you")
+# print("Replaced string3:", replaced_string3)
+
+# input_string = input("Enter a string with at least one '1': ")
+
+# modified_string = input_string.replace("1", "one")
+
+# print(modified_string)
+
+# message = input("Enter a message to shout: ")
+
+# shout_message = message.upper()
+
+# print(shout_message)
