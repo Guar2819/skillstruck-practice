3d418fc3c68f5a4db480a87fa8c35896d1b03dab
GuarA2819 <jguar312@evit.edu>
2025-02-13 16:57:30 -0700

    exported from Skill Struck

---
+++ checking_strings.py
@@ -0,0 +1,17 @@
+# string1 = "I love writing"
+# check = "love" in string1
+# print(check)
+
+# #outputs "True"
+
+# user_input = input("Enter a string: ")
+
+# contains_a = 'a' in user_input
+
+# print(contains_a)
+
+# user_input = input()
+
+# contains_1 = '1' in user_input
+
+# print(contains_1)
